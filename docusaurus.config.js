c1e7b9a52038fc612c70a4190e0036da6d91c303
aycanogut <aycanonfire@gmail.com>
2023-08-04 14:53:03 +0300

    chore: :pencil2: fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Frontend Mülakat soruları",
+        title: "Frontend Mülakat Soruları",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
